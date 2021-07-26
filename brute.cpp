1bea52f40d93b91fab6588bf385d208c4a187bd9
miyagi-sensei <60123481+miyagi-sensei@users.noreply.github.com>
2021-07-26 14:59:51 +0800

    Add files via upload

--- brute.cpp
+++ brute.cpp
@@ -1,41 +1,29 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 int R, C, N;
-vector< pair<int, int> > comb, best_comb;
 int Max_contrasts = -1;
+bool wall[100][100], best_wall[100][100];
 
-void print_wall() {
-  bool wall[100][100] = {false};
+void print_wall(bool w[][100]) {
   int i, j;
-  for (i=0; i<best_comb.size(); i++) {
-    int r = best_comb[i].first;
-    int c = best_comb[i].second;
-    wall[r][c] = true;
-  }
   for (i = 0; i < R; i++) {
     for (j = 0; j < C; j++) {
-      cout << wall[i][j];
+      cout << w[i][j];
     }
     cout << endl;
   }
 }
 
+// count the number of contrasts in the boolean 2D array wall
 int count_contrasts() {
-  int i, j, ans = 0;
-  bool map[100][100] = {false};
-
-  for (int i = 0; i < comb.size(); i++) {
-    map[comb[i].first][comb[i].second] = true;
-  }
-
+	int i, j, ans = 0;
   for (i = 0; i < R; i++) {
     for (j = 0; j < C; j++) {
-      if ((j != C-1) && (map[i][j] != map[i][j+1])) {
+      if ((j != C-1) && (wall[i][j] != wall[i][j+1])) {
         ans++;
       }
 
-      if ((i != R-1) && (map[i][j] != map[i+1][j])) {
+      if ((i != R-1) && (wall[i][j] != wall[i+1][j])) {
         ans++;
       }
     }
@@ -44,30 +32,28 @@ int count_contrasts() {
 }
 
 void combinations(int k, int offset) {
-  int i;
-  if (k==0) {
-    // print out that combination stored in comb vector
-    // for (i=0; i<comb.size(); i++)
-    // 	printf("(%d, %d) ", comb[i].first, comb[i].second);
-    // cout << endl;
-    int n_contrasts = count_contrasts();
-    if (n_contrasts > Max_contrasts) {
-      Max_contrasts = n_contrasts;
-      best_comb = comb;
-    }
-    return;
-  }
-  for (i=offset; i<R*C; i++) {
-    comb.push_back({i / C, i % C});
-    combinations(k-1, i+1);
-    comb.pop_back();
-  }
+	int i, j;
+	if (k==0) {
+		int n_contrasts = count_contrasts();
+		if (n_contrasts > Max_contrasts) {
+			Max_contrasts = n_contrasts;
+      for (i=0; i<R; i++)
+        for (j=0; j<C; j++)
+          best_wall[i][j] = wall[i][j];
+		}
+		return;
+	}
+	for (i=offset; i<R*C; i++) {
+    wall[i/C][i%C] = true;
+		combinations(k-1, i+1);
+    wall[i/C][i%C] = false;
+	}
 }
 
 int main() {
   cin >> R >> C >> N;
   combinations(N, 0);
   cerr << Max_contrasts << endl;
-  print_wall();
+  print_wall(best_wall);
   return 0;
 }
