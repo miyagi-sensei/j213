a736c4c5bcb60e85e355ad567c5d6f29689f0b06
miyagi-sensei <60123481+miyagi-sensei@users.noreply.github.com>
2021-07-26 15:25:41 +0800

    Add files via upload

---
+++ brute.py
@@ -0,0 +1,38 @@
+from itertools import combinations as comb
+from itertools import product
+
+
+def paint(black_cells):
+    wall = [[False] * C for _ in range(R)]
+    for r, c in black_cells:
+        wall[r][c] = True
+    return wall
+
+
+def count_contrasts(black_cells):
+    wall = paint(black_cells)
+    count = 0
+    for r, c in black_cells:
+        if r > 0:
+            count += (wall[r-1][c] != wall[r][c])
+        if r < R-1:
+            count += (wall[r+1][c] != wall[r][c])
+        if c > 0:
+            count += (wall[r][c-1] != wall[r][c])
+        if c < C-1:
+            count += (wall[r][c+1] != wall[r][c])
+    return count
+
+
+R, C, N = map(int, input().split())
+grid = product(range(R), range(C))
+max_contrasts = -1
+for black in comb(grid, N):
+    count = count_contrasts(black)
+    if count > max_contrasts:
+        max_contrasts = count
+        best_wall = paint(black)
+for row in best_wall:
+    for j in row:
+        print(int(j), end='')
+    print()
